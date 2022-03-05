e107492f9f1d9db1ee83b52695f87b0bb6581cfe
Tim Steeman <tmdev82@gmail.com>
2022-03-05 23:34:24 +0100

    added users endpoint and data

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route('/users', methods=["GET"])
+def users():
+    print("users endpoint reached...")
+    with open("users.json", "r") as f:
+        data = json.load(f)
+        data.append({
+            "username": "user4",
+            "pets": ["hamster"]
+        })
+
+        return flask.jsonify(data)
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
