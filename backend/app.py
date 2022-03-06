897be3ba92e4cbd308dea5895233fda1900d69c2
Tim Steeman <tmdev82@gmail.com>
2022-03-06 13:56:55 +0100

    add POST request handling in backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import flask
 import json
 from flask_cors import CORS
@@ -10,17 +10,27 @@ CORS(app)
 def hello():
     return "Hello, World!"
 
-@app.route('/users', methods=["GET"])
+@app.route('/users', methods=["GET", "POST"])
 def users():
     print("users endpoint reached...")
-    with open("users.json", "r") as f:
-        data = json.load(f)
-        data.append({
-            "username": "user4",
-            "pets": ["hamster"]
-        })
+    if request.method == "GET":
+        with open("users.json", "r") as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
 
-        return flask.jsonify(data)
+            return flask.jsonify(data)
+    if request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
